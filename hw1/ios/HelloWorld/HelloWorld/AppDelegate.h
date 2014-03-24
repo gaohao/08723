65e4eec2bda51fb9ba3b03c93d2488fabc1141a5
Hao Gao <hgao@twitter.com>
2014-03-24 02:12:17 -0400

    Initial Commit

---
+++ hw1/ios/HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Hao Gao on 3/24/14.
+//  Copyright (c) 2014 Hao Gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hw1/ios/HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Hao Gao on 3/24/14.
+//  Copyright (c) 2014 Hao Gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
